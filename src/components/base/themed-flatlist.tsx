7f8cf824499cc23b69d8f9d1699c58e989714d30
Kevin Ma <2613341+kevinma2010@users.noreply.github.com>
2025-08-05 23:27:48 +0800

    feat: add ThemedFlatList component with theme support

---
+++ src/components/base/themed-flatlist.tsx
@@ -0,0 +1,19 @@
+import { FlatList, type FlatListProps } from 'react-native';
+
+import { useThemeColor } from '@/hooks/use-theme-color';
+
+export type ThemedFlatListProps<ItemT> = FlatListProps<ItemT> & {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export function ThemedFlatList<ItemT>({
+  style,
+  lightColor,
+  darkColor,
+  ...otherProps
+}: ThemedFlatListProps<ItemT>) {
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <FlatList style={[{ backgroundColor }, style]} {...otherProps} />;
+}
